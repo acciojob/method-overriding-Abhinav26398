b5a70d2877225c6f97b0cecec6e1feab3b3c1415
Abhinav Kumar Jha <abhinavjha26398@gmail.com>
2022-10-10 19:03:41 +0530

    method overriding

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -5,20 +5,21 @@ import org.junit.jupiter.api.Test;
 
 public class TestCases {
     public static void main(String[] args) {
-        com.driver.B obj=new com.driver.B();
+        class A {
+            public String meth(){
+                return "Invoking method from class A";
+            }
+        }
+        class B extends A{
+            public String meth(){
+                return "Method is overridden in Extendend class B";
+            }
+        }
+        B obj=new B();
         String ans= obj.meth();
         System.out.println(ans);
         String ans2= obj.meth();
         System.out.println(ans2);
     }
 }
-class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends com.driver.A {
-    public String meth(){
-        return "Invoking method from class B";
-    }
-}
+
